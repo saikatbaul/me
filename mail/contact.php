bad198a7a13492729f17d893c997f4a9619ea9bf
Saikat Baul <saikat.sb67@gmail.com>
2023-02-21 18:15:08 +0600

    1st

---
+++ mail/contact.php
@@ -0,0 +1,20 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "saikatbaul.cs@gmail.com"; // Change this email to your //
+$subject = "$m_subject:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$headers = "From: $email";
+$headers .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $headers))
+  http_response_code(500);
+?>
